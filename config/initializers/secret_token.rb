3136e66aa8b4307d034577002a38c39735181487
Mike Chen <chagel@gmail.com>
2014-03-11 19:47:54 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pom::Application.config.secret_key_base = '19f50a73d8506debb00cf438cafff36d597ed94d3f2b0ca2bff757844f5db0ae01333a34fb4168fe40bd941d29679c2ac5888bff377bbb4044372694362e184a'
