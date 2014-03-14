aaeb3922327fa6e59aeb208823261a8a8eb54bb2
Daneil Wei <daneilmu1@gmail.com>
2014-03-14 17:30:13 +0800

    add filters -> radio_button and scope action

--- app/admin/income.rb
+++ app/admin/income.rb
@@ -1,21 +1,30 @@
 ActiveAdmin.register Income do
+
   config.sort_order = "dim_date_id_desc"
   config.clear_action_items!
   csv do
     column :dim_date_id
     column :dim_income_source
     column :total_coins
+    column :total_gems
   end
+  scope :all
+  scope :coins_only
+  scope :gems_only
+  
   index do
     column :dim_date
     column :dim_income_source
     column :total_coins
-    # column :total_gems
+    column :total_gems
   end
 
-  filter :dim_date
+  filter :dim_date_id_eq, :as => :radio, :collection => [['today', Date.today.strftime('%Y%m%d')], ['yesterday', Date.yesterday.strftime('%Y%m%d')], 
+  ['any' ,'Any']], :label => 'dim_date'
+
+  #filter :dim_date
   filter :dim_game
   filter :dim_income_source
-  # filter :total_gems
+  filter :total_gems
   filter :total_coins
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -3,8 +3,7 @@ class Income < ActiveRecord::Base
   belongs_to :dim_date
   belongs_to :dim_game
   belongs_to :dim_income_source
-  scope :coins_only, lambda {|option| where('total_coins > 0') if option}
-  scope :gems_only, lambda {|option| where('total_gems > 0') if option}
-
-  # search_method :starfish_only
+  scope :coins_only, where('total_coins > 0')
+  scope :gems_only,  where('total_gems > 0')
+  #search_method :dim_date_id_eq
 end
