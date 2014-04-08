7b038fa880006fa429f0a25859e4fe68879fc096
Daneil Wei <daneilmu1@gmail.com>
2014-04-08 13:40:46 +0800

    solve scope today and yesterday

--- app/models/income.rb
+++ app/models/income.rb
@@ -5,6 +5,7 @@ class Income < ActiveRecord::Base
   belongs_to :dim_income_source
   scope :coins_only, where('total_coins > 0')
   scope :gems_only,  where('total_gems > 0')
-  scope :today, where("dim_date_id=#{Date.today.strftime('%Y%m%d')}")
-  scope :yesterday, where("dim_date_id=#{Date.yesterday.strftime('%Y%m%d')}")
+  
+  scope :today, -> { where(dim_date_id: Date.today.strftime('%Y%m%d'))}
+  scope :yesterday, -> { where(dim_date_id: Date.yesterday.strftime('%Y%m%d'))}
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,7 +4,7 @@ class Purchase < ActiveRecord::Base
   belongs_to :dim_game
   scope :coins_only, where('total_coins > 0')
   scope :gems_only,  where('total_gems > 0')
-  scope :today, where("dim_date_id=#{Date.today.strftime('%Y%m%d')}")
-  scope :yesterday, where("dim_date_id=#{Date.yesterday.strftime('%Y%m%d')}")
-  # search_method :starfish_only
+  #scope :today, -> { where("dim_date_id=#{Date.today.strftime('%Y%m%d')}") }
+  scope :today, -> { where(dim_date_id: Date.today.strftime('%Y%m%d'))}
+  scope :yesterday, -> { where(dim_date_id: Date.yesterday.strftime('%Y%m%d'))}
 end
